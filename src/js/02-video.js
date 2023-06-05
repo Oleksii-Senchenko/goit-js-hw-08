150e9798f90bf910e825a6d4f781bf0b11df699e
Oleksii-Senchenko <106326629+Oleksii-Senchenko@users.noreply.github.com>
2023-06-05 15:08:52 +0300

    hw8 Done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+
+const LOCAL_KEY = "videoplayer-current-time"
+
+const iframeEl = document.querySelector('iframe')
+const player = new Player(iframeEl);
+
+
+
+ function updateTime({seconds}) {
+    localStorage.setItem(LOCAL_KEY, seconds)
+}
+
+
+player.setCurrentTime(localStorage.getItem(LOCAL_KEY) || 0)
+
+player.on('timeupdate', throttle(updateTime, 1000) );
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,99 @@
+import throttle from 'lodash.throttle';
+//нашел форму
+const formEl = document.querySelector('.feedback-form')
 
+
+
+
+// const save = (key, value) => {
+//     try {
+//       const serializedState = JSON.stringify(value);
+//       localStorage.setItem(key, serializedState);
+//     } catch (error) {
+//       console.error("Set state error: ", error.message);
+//     }
+//   };
+
+//   const load = key => {
+//     try {
+//       const serializedState = localStorage.getItem(key);
+//       return serializedState === null ? undefined : JSON.parse(serializedState);
+//     } catch (error) {
+//       console.error("Get state error: ", error.message);
+//     }
+//   };
+
+
+
+
+
+
+const LOCAL_KEY = "feedback-form-state"
+
+let userData = {}
+
+
+function onInputChange({ target }) {
+
+    const inputValue = target.value
+ 
+    const inputName = target.name
+
+    userData[inputName] = inputValue
+
+
+    localStorage.setItem(LOCAL_KEY, JSON.stringify(userData))
+}
+
+
+
+
+
+function onRenderPage(){
+
+    const unPars = JSON.parse(localStorage.getItem(LOCAL_KEY))
+
+    if (!unPars) {
+        return
+    }
+
+    for (const key in unPars) {
+        if (unPars.hasOwnProperty(key)) {
+                            //   value это textContent
+          formEl.elements[key].value = unPars[key];
+
+          if (unPars[key]) {
+            
+            userData[key] = unPars[key]
+
+          }     
+        }
+    }
+}
+function onSubmit(e){
+e.preventDefault()
+
+const {elements:{email, message}} = e.currentTarget
+if (email.value === '' || message.value === '') {
+     
+alert('Fill All Fields')
+
+    return
+
+}
+
+localStorage.removeItem(LOCAL_KEY)
+e.currentTarget.reset()
+console.log(userData);
+userData = {}
+}
+
+
+
+
+
+onRenderPage()
+
+
+formEl.addEventListener('input', throttle(onInputChange, 500))
+formEl.addEventListener('submit', onSubmit)
